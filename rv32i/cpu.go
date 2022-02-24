3d8007eca2a50d6a2677437d9086b9dbd1cd10b2
kinpoko <79001545+kinpoko@users.noreply.github.com>
2022-02-24 18:08:14 +0900

    Add ID stage

--- rv32i/cpu.go
+++ rv32i/cpu.go
@@ -6,6 +6,12 @@ type Cpu struct {
 	Exit     uint32
 }
 
+type Instruction struct {
+	Rs1 uint8
+	Rs2 uint8
+	Rd  uint8
+}
+
 func MovePc(cpu Cpu, addr uint32) Cpu {
 	cpu.Pc = cpu.Pc + addr
 	return cpu
@@ -14,4 +20,13 @@ func MovePc(cpu Cpu, addr uint32) Cpu {
 func SetExit(cpu Cpu, exit uint32) Cpu {
 	cpu.Exit = exit
 	return cpu
+}
+
+func Decode(bits uint32) Instruction {
+	var inst Instruction
+	inst.Rs1 = uint8((bits & 0x000F8000) >> 15)
+	inst.Rs2 = uint8((bits & 0x01F00000) >> 20)
+	inst.Rd = uint8((bits & 0x00000F80) >> 7)
+
+	return inst
 }
--- rv32i/run.go
+++ rv32i/run.go
@@ -2,25 +2,6 @@ package rv32i
 
 import "fmt"
 
-func Loop(cpu Cpu, memory Memory, debug bool) error {
-	var inst uint32
-	for {
-		inst = uint32(memory.Memory[cpu.Pc]) | uint32(memory.Memory[cpu.Pc+1])<<8 | uint32(memory.Memory[cpu.Pc+2])<<16 | uint32(memory.Memory[cpu.Pc+3])<<24
-
-		if debug {
-			fmt.Printf("pc   : 0x%x\n", cpu.Pc)
-			fmt.Printf("inst : 0x%x\n", inst)
-			fmt.Print("------\n")
-		}
-		if inst == cpu.Exit {
-			break
-		}
-
-		cpu = MovePc(cpu, 4)
-	}
-	return nil
-}
-
 func Run(binary []byte, debug bool) error {
 	var cpu Cpu
 	cpu = SetExit(cpu, 0x34333231)
@@ -32,5 +13,33 @@ func Run(binary []byte, debug bool) error {
 		return err
 	}
 
+	return nil
+}
+
+func Loop(cpu Cpu, memory Memory, debug bool) error {
+	var bits uint32
+	for {
+		// Instruction Fetch
+		bits = uint32(memory.Memory[cpu.Pc]) | uint32(memory.Memory[cpu.Pc+1])<<8 | uint32(memory.Memory[cpu.Pc+2])<<16 | uint32(memory.Memory[cpu.Pc+3])<<24
+
+		// Instruction Decode
+		inst := Decode(bits)
+
+		if debug {
+			fmt.Printf("pc   	 : 0x%x\n", cpu.Pc)
+			fmt.Printf("inst 	 : 0x%x\n", bits)
+			fmt.Printf("rs1  	 : %d\n", inst.Rs1)
+			fmt.Printf("rs2  	 : %d\n", inst.Rs2)
+			fmt.Printf("rd  	 : %d\n", inst.Rd)
+			fmt.Printf("rs1_data : 0x%x\n", cpu.Register[inst.Rs1])
+			fmt.Printf("rs2_data : 0x%x\n", cpu.Register[inst.Rs2])
+			fmt.Print("------\n")
+		}
+		if bits == cpu.Exit {
+			break
+		}
+
+		cpu = MovePc(cpu, 4)
+	}
 	return nil
 }
